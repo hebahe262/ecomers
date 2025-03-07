94d4af85848fa88f6dbf1366c9168cef7d82bfbf
heba55 <gogoalimohamed300@gmail.com>
2025-03-07 05:50:41 +0200

    headers

--- src/app/core/interceptors/headers/hedars.interceptor.ts
+++ src/app/core/interceptors/headers/hedars.interceptor.ts
@@ -1,16 +1,23 @@
+import { isPlatformBrowser } from '@angular/common';
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject, PLATFORM_ID } from '@angular/core';
 
 export const hedarsInterceptor: HttpInterceptorFn = (req, next) => {
 // logic req
-if(localStorage.getItem('userToken')){
-  if(req.url.includes('cart')|| req.url.includes('orders')||req.url.includes('wishlist')){
-    req = req.clone({
-      setHeaders:{
-        token:localStorage.getItem('userToken')!
-      }
-    })
+
+const  _pLATFORM_ID=inject( PLATFORM_ID)
+if(isPlatformBrowser(_pLATFORM_ID)){
+  if(localStorage.getItem('userToken')){
+    if(req.url.includes('cart')|| req.url.includes('orders')||req.url.includes('wishlist')){
+      req = req.clone({
+        setHeaders:{
+          token:localStorage.getItem('userToken')!
+        }
+      })
+    }
   }
 }
+
 
   return next(req);  // logic resbons
 };
