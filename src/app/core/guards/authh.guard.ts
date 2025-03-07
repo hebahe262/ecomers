aec1c337a6e0c26ebbb1536118027d67708b2005
heba55 <gogoalimohamed300@gmail.com>
2025-03-07 06:04:51 +0200

    edite

--- src/app/core/guards/authh.guard.ts
+++ src/app/core/guards/authh.guard.ts
@@ -2,16 +2,16 @@ import { isPlatformBrowser } from '@angular/common';
 import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-import{ jwtDecode}from'jwt-decode'
+// import{ jwtDecode}from'jwt-decode'
 
 export const authhGuard: CanActivateFn = (route, state) => {
   const _router=inject(Router)
   const _pLATFORM_ID=inject(PLATFORM_ID)
  
   if(isPlatformBrowser(_pLATFORM_ID)){
-    const token =localStorage.getItem('userToken')!;
+    // const token =localStorage.getItem('userToken')!;
     //  console.log( (jwtDecode(localStorage.getItem('userToken')!)as {id:string}).id)
-    localStorage.setItem('userId',(jwtDecode(token)as {id:string}).id)
+    // localStorage.setItem('userId',(jwtDecode(token)as {id:string}).id)
     if(localStorage.getItem('userToken') !== null){
       return true;
      }
